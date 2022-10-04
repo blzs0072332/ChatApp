e0cbc8907fa6961562e5365133e8039cb89b752b
hallgato <hallgato@LABOR215-16>
2022-10-04 08:57:14 +0200

    Create view model and inport RestCollection class

---
+++ ChatGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+
+namespace ChatGUI.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        private string sender;
+        private string inputMessage;
+        private List<string> messages;
+
+        public string Sender { get => sender; set => sender = value; }
+        public string InputMessage
+        {
+            get { return inputMessage; }
+            set { inputMessage = value; OnPropertyChanged(); (SendCommand as RelayCommand).NotifyCanExecuteChanged(); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+    }
+}
