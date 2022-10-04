221b67611684862dabddcd3794bbc34382ce8455
hallgato <hallgato@LABOR215-16>
2022-10-04 08:13:15 +0200

    Add project files.

---
+++ ChatServer/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.Models
+{
+    public class Message
+    {
+        string msg;
+        DateTime date;
+        string sender;
+    }
+}
